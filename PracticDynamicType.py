9a25207594adf776b42c759a59d4ea53f093d6bc
Адиль Неталиев <164340754+Orenonawaa@users.noreply.github.com>
2024-09-09 00:27:16 +0300

    Add files via upload

---
+++ PracticDynamicType.py
@@ -0,0 +1,12 @@
+name  = 'Адиль'
+print("Name: " + name)
+age = 24
+age1 = (str(age))
+print("Age: " + age1)
+age = 52
+age2 = (str(age))
+print("New age: " + age1)
+is_student = True
+is_student1 = (str(is_student))
+print("Is Student: " + is_student1)
+
