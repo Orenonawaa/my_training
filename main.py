e9262e41bb9497436ded4c809ad661fa046860b9
Адиль Неталиев <164340754+Orenonawaa@users.noreply.github.com>
2024-09-08 18:57:35 +0300

    08/09 Выполнил Практическое задание №1 в модуле: "Python введение"

---
+++ main.py
@@ -0,0 +1,25 @@
+# 1st program
+
+print(5 * (9 ** 0.5))
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a == b)
+
+# 4th program
+
+a = '123.456'
+b = (float(a))
+print(type(b))
+c = b * 10
+print(c)
+d = (int(c))
+print(d)
+e = d % 10
+print(e)
