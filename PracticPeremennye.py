0bbbdc3477b405e9d3f7ec6a6e40a9caf30c31ad
Адиль Неталиев <164340754+Orenonawaa@users.noreply.github.com>
2024-09-08 23:47:15 +0300

    Add files via upload

---
+++ PracticPeremennye.py
@@ -0,0 +1,8 @@
+count_complete_home_task = 12
+count_complete_home_task1 = (str(count_complete_home_task))
+count_spending_time = 1.5
+count_spending_time1 = (str(count_spending_time))
+course_name = 'Python'
+timeAtOneTask = count_spending_time / count_complete_home_task
+timeAtOneTask1 = (str(timeAtOneTask))
+print("Курс: " + course_name + ", всего задач: " + count_complete_home_task1 + ", затрачено часов: " + count_spending_time1 + ", среднее время выполнения " + timeAtOneTask1 + " часа")
