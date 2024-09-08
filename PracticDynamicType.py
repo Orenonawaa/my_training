c530b2a5dd98adcc4582bd567f32f08383573de1
Адиль Неталиев <164340754+Orenonawaa@users.noreply.github.com>
2024-09-09 00:33:00 +0300

    Update PracticDynamicType.py

--- PracticDynamicType.py
+++ PracticDynamicType.py
@@ -3,10 +3,9 @@ print("Name: " + name)
 age = 24
 age1 = (str(age))
 print("Age: " + age1)
-age = 52
-age2 = (str(age))
+age = age + 1
+age1 = (str(age))
 print("New age: " + age1)
 is_student = True
 is_student1 = (str(is_student))
 print("Is Student: " + is_student1)
-
