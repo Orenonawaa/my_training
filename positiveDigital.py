5e69f3c956962f812e38763ab9331cfab3c5442c
Адиль Неталиев <164340754+Orenonawaa@users.noreply.github.com>
2024-09-21 17:26:09 +0300

    Update positiveDigital.py

--- positiveDigital.py
+++ positiveDigital.py
@@ -1,11 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-positive_numbers = []
-
 x = 0
 while x < len(my_list):
-    if my_list[x] <= 0:
-        x += 1
+    if my_list[x] < 0:
         continue
-    positive_numbers.append(my_list[x])
+    print(my_list[x])
     x += 1
-print(positive_numbers)
