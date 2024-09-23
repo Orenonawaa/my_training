8757e276272ef3dea0c965668d43faf55ca39577
Адиль Неталиев <164340754+Orenonawaa@users.noreply.github.com>
2024-09-23 22:26:30 +0300

    Update positiveDigital.py

--- positiveDigital.py
+++ positiveDigital.py
@@ -2,7 +2,11 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 x = 0
 while x < len(my_list):
-    if my_list[x] <= 0:
+    if my_list[x] > 0:
+        print(my_list[x])
+        x += 1
+    elif my_list[x] == 0:
+        x += 1
+        continue
+    else:
         break
-    print(my_list[x])
-    x += 1
